ff675080c13703599fac0272fe27aba44d36b3b5
Vishesh Handa <me@vhanda.in>
2021-04-20 09:50:26 +0200

    fix create table
    Also use Array of Tuples for the extra properties. Ideally it would be
    perfect to use Maps but those are stil experimental, and we can always
    convert the data later. I hope.

--- create.sql
+++ create.sql
@@ -1,37 +1,39 @@
 /* CREATE DATABASE IF NOT EXISTS gitjournal */
 CREATE TABLE events
 (
-    `timestamp` DateTime('UTC'),
-    `name` STRING,
-    `params` STRING, /* how to store this? */
-    `previous_timestamp`: DateTime('UTC'),
-    `bundle_sequence_id`: Int32,
+  `timestamp` DateTime('UTC'),
+  `name` String,
+  `params` Array(Tuple(String, String)),
+  `previous_timestamp` DateTime('UTC'),
+  `bundle_sequence_id` Int32,
 
-    `user_pseudo_id` UUID,
-    `user_properties` STRING, /* how to store this? */
+  `user_pseudo_id` UUID,
+  `user_properties` Array(Tuple(String, String)),
 
-    `device.category` LowCardinality(STRING),
-    `device.mobile_brand_name` LowCardinality(STRING),
-    `device.mobile_model_name` LowCardinality(STRING),
-    `device.mobile_marketing_name` LowCardinality(STRING),
-    `device.mobile_os_hardware_model` LowCardinality(STRING),
-    `device.operating_system` LowCardinality(STRING),
-    `device.operating_system_version` LowCardinality(STRING),
-    `device.language` LowCardinality(STRING),
-    `device.time_zone_offset_seconds` Int16,
+  /* Should I use nested instead - Not supported by offical go driver */
+  `device.category` LowCardinality(String),
+  `device.mobile_brand_name` LowCardinality(String),
+  `device.mobile_model_name` LowCardinality(String),
+  `device.mobile_marketing_name` LowCardinality(String),
+  `device.mobile_os_hardware_model` LowCardinality(String),
+  `device.operating_system` LowCardinality(String),
+  `device.operating_system_version` LowCardinality(String),
+  `device.language` LowCardinality(String),
+  `device.time_zone_offset_seconds` Int16,
 
-    `geo.continent` LowCardinality(STRING),
-    `geo.country` LowCardinality(STRING),
-    `geo.region` LowCardinality(STRING),
-    `geo.city` LowCardinality(STRING),
-    `geo.sub_continent` LowCardinality(STRING),
-    `geo.metro` LowCardinality(STRING),
+  `geo.continent` LowCardinality(String),
+  `geo.country` LowCardinality(String),
+  `geo.region` LowCardinality(String),
+  `geo.city` LowCardinality(String),
+  `geo.sub_continent` LowCardinality(String),
+  `geo.metro` LowCardinality(String),
 
-    `app_info.id` LowCardinality(STRING),
-    `app_info.version` LowCardinality(STRING),
+  `app_info.id` LowCardinality(String),
+  `app_info.version` LowCardinality(String),
 
-    `stream_id` Int64,
-    `platform` LowCardinality(STRING)
+  `stream_id` Int64,
+  `platform` LowCardinality(String)
 )
 ENGINE = MergeTree()
 PARTITION BY toYYYYMM(`timestamp`)
+ORDER BY (`timestamp`)
