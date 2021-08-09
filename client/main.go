b6308cecd884001d01577d04319a962d6f58b403
Vishesh Handa <me@vhanda.in>
2021-08-09 17:22:26 +0200

    client: Try to connect to the deployed server
    This also fails. So it's not just my dart code, something else is wrong.

--- client/main.go
+++ client/main.go
@@ -2,27 +2,43 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"log"
 	"time"
 
 	pb "github.com/gitjournal/analytics_backend/protos"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 const (
-	address = "127.0.0.1:8080"
+	// address = "127.0.0.1:8080"
+	address = "https://analyticsbackend-wetu2tkdpq-ew.a.run.app:8080"
 )
 
 func main() {
-	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	// conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	var opts []grpc.DialOption
+	systemRoots, err := x509.SystemCertPool()
+	if err != nil {
+		log.Fatal(err)
+	}
+	cred := credentials.NewTLS(&tls.Config{
+		RootCAs: systemRoots,
+	})
+	opts = append(opts, grpc.WithTransportCredentials(cred))
+	opts = append(opts, grpc.WithBlock())
+
+	conn, err := grpc.Dial(address, opts...)
 	if err != nil {
 		log.Printf("Failed to dial: %v", err)
 	}
 
 	defer conn.Close()
 
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	client := pb.NewAnalyticsServiceClient(conn)
